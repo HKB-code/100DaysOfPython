833bb1b622ca6fc48b3158e6ae052fcf577fff4c
HkbCodes <harshilj2002@gmail.com>
2024-09-03 23:13:39 +0530

    Day6-part2

--- Day6/test.py
+++ Day6/test.py
@@ -2,3 +2,10 @@ def my_Function():
   print("Hello")
   
 my_Function()
+
+
+number=6
+while number>0:
+  print("hi")
+  number-=1
+  # Decrement Operation: number-- is not valid in Python. This syntax is used in languages like C, C++, and Java.
