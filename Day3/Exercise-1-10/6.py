9e41931d33ebe3783bfa433da3dc68753db19817
HkbCodes <harshilj2002@gmail.com>
2024-09-13 18:42:56 +0530

    comment add

--- Day3/Exercise-1-10/6.py
+++ Day3/Exercise-1-10/6.py
@@ -2,3 +2,4 @@
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 # d  has an index of 3  here, e  has an index of 4 , and f  has an index of 5, but since the slicing syntax is upper-bound exclusive, we need to pass 6  as the upper bound.
 print(letters[3:6])
+# the term upper-bound exclusive means that the upper limit specified in a range or sequence is not included in the output. 
